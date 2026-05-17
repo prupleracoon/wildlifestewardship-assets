b88a34eeeed2bd8184ea5d92dbabad6bfcd261db
prupleracoon <prupleracoon@gmail.com>
2026-05-16 21:16:25 -0700

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,18 @@
+<script>
+    function openTab(evt, tabName) {
+        let i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active");
+    }
+
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
+</script>
